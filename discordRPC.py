f790f6e34fad8597145339704c076b1e35717387
浪人 <mighxi@yahoo.com>
2019-02-09 04:22:35 +0300

    v0.1

---
+++ discordRPC.py
@@ -0,0 +1,31 @@
+from time import sleep
+from selenium import webdriver
+from pypresence import Presence
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+client_id = ''  
+RPC = Presence(client_id, pipe=0)
+RPC.connect() 
+
+chrome_options = Options()
+dc = DesiredCapabilities.CHROME
+dc['loggingPrefs'] = { 'browser':'ALL' }
+chrome_options.add_experimental_option('debuggerAddress', '127.0.0.1:9222')
+driver = webdriver.Chrome(chrome_options = chrome_options, desired_capabilities = dc)
+
+with open("./youtube.js", "r") as x:
+    js = x.read()
+    x.close()
+
+while True:
+    try:
+        driver.execute_script(js)
+        logdriver = driver.get_log('browser')
+        vtime = logdriver[-1]['message'][20:-1]
+        RPC.update(large_image = 'youtube', large_text = 'Youtube', details = driver.title[:-10], state = vtime)
+        sleep(1)
+    except Exception as e:
+        print(str(e))
+        sleep(10)
+        continue
